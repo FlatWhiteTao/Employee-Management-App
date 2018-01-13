4a26d143148dcf1a0922bc314d86a2edb6850a52
Tao Wang <tao.wangtaoau@gmail.com>
2018-01-13 22:27:41 +1100

    Added Auth-action and Auth-reducer

---
+++ EmployeeMgn/src/reducers/AuthReducer.js
@@ -0,0 +1,9 @@
+// AuthReducer is responsible for all logic regrading the Auth
+const INITIAL_STATE = { email: '' };
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};

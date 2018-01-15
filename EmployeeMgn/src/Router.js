dc8ebd30171ecd00840f569c2979297f35a84e01
Tao Wang <tao.wangtaoau@gmail.com>
2018-01-15 17:33:28 +1100

    Imported router-flux and using a main router support app navigations

---
+++ EmployeeMgn/src/Router.js
@@ -0,0 +1,17 @@
+// define all the differnet routers
+
+import React from 'react';
+import { Scene, Router } from 'react-native-router-flux';
+import LoginForm from './components/LoginForm';
+
+const RouterComponent = () => {
+  return (
+    <Router>
+      <Scene key="root">
+        <Scene key="login" component={LoginForm} title="PLease Login" />
+      </Scene>
+    </Router>
+  );
+};
+
+export default RouterComponent;

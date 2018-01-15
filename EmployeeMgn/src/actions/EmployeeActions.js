1dc44ae7158311d57095090619678f62824c2709
Tao Wang <tao.wangtaoau@gmail.com>
2018-01-15 23:37:21 +1100

    Added a employee creation form with redux fasion

---
+++ EmployeeMgn/src/actions/EmployeeActions.js
@@ -0,0 +1,11 @@
+// on action creator to update any specific prop
+import {
+  EMPLOYEE_UPDATE
+} from './types';
+
+export const employeeUpdate = ({ prop, value }) => {
+  return {
+    type: EMPLOYEE_UPDATE,
+    payload: { prop, value }
+  };
+};

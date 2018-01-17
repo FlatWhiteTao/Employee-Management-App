6303ee89f8f00454310b3ae142a64971e0e62ab9
Tao Wang <tao.wangtaoau@gmail.com>
2018-01-18 00:58:24 +1100

    Using componentWillReceiveProps (nextProps) to solve the asyn call problem

--- EmployeeMgn/src/components/EmployeeList.js
+++ EmployeeMgn/src/components/EmployeeList.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ListView, View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
   componentWillMount() {
+    // asyn call -- request to the firebase
     this.props.employeesFetch();
+    this.createDataSource(this.props);
   }
+
+  componentWillReceiveProps(nextProps) {
+    // nextProps ARE THE NEXT SET OF props
+    // that this component will be
+    // rendered with
+    // this.props is still the old set of prpos
+
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employess }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(employess);
+  }
+
+
   render() {
     return (
       <View>

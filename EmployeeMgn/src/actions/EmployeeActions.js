b8221b560129e9d6271bdce4e13dfc7619696fc2
Tao Wang <tao.wangtaoau@gmail.com>
2018-01-17 23:59:57 +1100

    navigate back to the list after adding one

--- EmployeeMgn/src/actions/EmployeeActions.js
+++ EmployeeMgn/src/actions/EmployeeActions.js
@@ -1,5 +1,6 @@
 // on action creator to update any specific prop
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import {
   EMPLOYEE_UPDATE
 } from './types';
@@ -15,6 +16,10 @@ export const employeeCreate = ({ name, phone, shift }) => {
   // define a path to a json store
   const { currentUser } = firebase.auth();
   // get the reference to the location of our database store
-  firebase.database().ref(`/users/${currentUser.uid}/employees`)
-    .push({ name, phone, shift });
+
+  return () => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift })
+      .then(() => Actions.pop());
+  };
 };

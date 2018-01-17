ece59b1a5eca6c2fba6a8f0ea6b822ef8e522c38
Tao Wang <tao.wangtaoau@gmail.com>
2018-01-18 01:33:40 +1100

    transferring the json object to an array and render it to the list view

--- EmployeeMgn/src/components/EmployeeList.js
+++ EmployeeMgn/src/components/EmployeeList.js
@@ -1,7 +1,9 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import { ListView, View, Text } from 'react-native';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
 import { employeesFetch } from '../actions';
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
   componentWillMount() {
@@ -19,26 +21,36 @@ class EmployeeList extends Component {
     this.createDataSource(nextProps);
   }
 
-  createDataSource({ employess }) {
+  createDataSource({ employees }) {
     const ds = new ListView.DataSource({
       rowHasChanged: (r1, r2) => r1 !== r2
     });
 
-    this.dataSource = ds.cloneWithRows(employess);
+    this.dataSource = ds.cloneWithRows(employees);
   }
 
+  renderRow(employee) {
+    return <ListItem employee={employee} />;
+  }
 
   render() {
+  console.log(this.props);
     return (
-      <View>
-        <Text> Mushi </Text>
-        <Text> Mushi </Text>
-        <Text> Mushi </Text>
-        <Text> Mushi </Text>
-        <Text> Mushi </Text>
-      </View>
+      <ListView
+        enableEmptySections
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
     );
   }
 }
 
-export default connect(null, { employeesFetch })(EmployeeList);
+const mapStateToProps = state => {
+  const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, uid };
+  });
+
+  return { employees };
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
